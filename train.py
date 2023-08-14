b06cb863e7b43788c2077f067cd09beb958dab73
Adhi Setiawan <adhisetiawan518@gmail.com>
2023-08-14 20:45:01 +0700

    define environment

--- train.py
+++ train.py
@@ -23,7 +23,25 @@ def train(n_training_episodes, min_epsilon, max_epsilon, decay_rate, env, max_st
     return Qtable
 
 if __name__ == "__main__":
-    # Your training configuration and Q-table initialization here
-    # ...
+    n_training_episodes = 10000     # total training episode
+    learning_rate = 0.7             # learning rate
+
+    n_eval_episodes = 100           # total test episode
+
+    env_id = "FrozenLake-v1"        # environment name
+    max_steps = 99                  # max step per episode
+    gamma = 0.95                    # discount rate
+    eval_seed = []                  # eval seed environment
+
+    max_epsilon = 1.0               # exploration probability at start
+    min_epsilon = 0.05              # minimum exploration probability
+    decay_rate = 0.0005             # exponential decay rate for exploration probability
+
+    env = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=False, render_mode="rgb_array")
+
+    state_space = env.observation_space.n
+    action_space = env.action_space.n
+
+    Qtable_frozenlake = initialize_q_table(state_space, action_space)
 
     Qtable_frozenlake = train(n_training_episodes, min_epsilon, max_epsilon, decay_rate, env, max_steps, Qtable_frozenlake)
