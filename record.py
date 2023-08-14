de870033110f7d9d1173f93686a50e24221f7228
Adhi Setiawan <adhisetiawan518@gmail.com>
2023-08-14 20:50:24 +0700

    delete main in record.py

--- record.py
+++ record.py
@@ -18,9 +18,3 @@ def record_video(env, Qtable, out_directory, fps=1):
         images.append(img)
         
     imageio.mimsave(out_directory, [np.array(img) for i, img in enumerate(images)], fps=fps)
-
-if __name__ == "__main__":
-    # Your recording configuration and Q-table loading here
-    # ...
-
-    record_video(env, Qtable_frozenlake, "/home/adhi/reinforcement-learning-exploration/huggingface/unit2/replay.mp4", fps=1)
